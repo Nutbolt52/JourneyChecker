7080d08fa24250a1e72d02ef8e800f725c265c55
Nutbolt52 <adam@nutbolt.eu>
2014-06-02 08:48:54 +0100

    Fixed Logo Size
    Made logo more centered and larger. Fitting in with the navbar better.

--- inc/menu.php
+++ inc/menu.php
@@ -8,7 +8,7 @@
       $header .= '<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">';
       $header .= '<span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>';
       //Branding
-      $header .= '<div class="navbar-form" style="margin-top:0px; margin-bottom: 3.5px; padding-left:0px; padding-right:0px"><a class="navbar-left" href="/"><img src="img/icon_orange.png" style="height:45px; padding:5px" alt="Journey Checker"></a>';
+      $header .= '<div class="navbar-form" style="margin-top:0px; margin-bottom: 3.5px; padding-left:0px; padding-right:0px"><a class="navbar-left" href="/"><img src="img/icon_orange.png" style="height:50px; padding:5px" alt="Journey Checker"></a>';
       $header .= '<a class="navbar-brand" href="/"> Journey Checker</a></div></div>';
       //Collapseable Menu
       $header .= '<div class="collapse navbar-collapse">';
