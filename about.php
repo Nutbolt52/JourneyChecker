2b5fbb458d840fbc19976bc9b4f603fa90751806
Nutbolt52 <adam@nutbolt.eu>
2014-06-27 07:24:02 +0100

    Updated About Us Page
    Updated the text to be more relevant with current state of JC

--- about.php
+++ about.php
@@ -43,8 +43,10 @@
         </div>
         <div class="well">
             <p>The Journey Checker was developed by Nutbolt as a way of quickly and easily checking if there were any delays on his journey home from work. </p>
-            <p>It grew to allow more flexibility, allowing users to select their own stations and tube lines etc...</p>
+            <p>It grew to allow more flexibility, allowing users to select their own tube lines etc...</p>
             <p>The London Underground data is provided by TfL and is updated every 30 seconds</p>
+            <hr>
+            <p>if you find an issue with the website, or would like to contact me for any other reason, please use the contact form which will appear when I finish making it...</p>
             
         </div>
         
