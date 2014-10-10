9835300aa381a239f9ce4431e88e8fb79fb4c590
Nutbolt52 <adam@nutbolt.eu>
2014-10-10 10:26:34 +0100

    Fix Cache file location hotfix
    A simple hotfix to resolve a small issue and make code neater

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
   defined('IN_APP') || die("Hands off!");
 
   //Create TfL Cache location string
-  $tflcachelocation = $_SERVER['DOCUMENT_ROOT'] . '/tflcache.xml.cache';
+  $tflcachelocation = ($_SERVER['DOCUMENT_ROOT'] . '/tflcache.xml.cache');
   
   //TfL Data Options
   define('TFL_URL', 'http://cloud.tfl.gov.uk/TrackerNet/LineStatus'); 
