61121151d7038753b47e9265434bb0f4e2c71a75
Nutbolt52 <adam@nutbolt.eu>
2015-02-04 19:01:10 +0000

    Add More Information button
    Have added a button the home page if no preferences are set. This
    provides more information about the website, and will be useful for
    keywords and indexing by search engines.
    
    Also changed the auto-update to 32seconds in an attempt to fix a refresh
    issue.

--- about.php
+++ about.php
@@ -47,7 +47,7 @@
             <p>The Journey Checker was developed by Nutbolt as a way of quickly and easily checking if there were any delays on his journey home from work</p>
             <p>It grew to allow more flexibility, allowing users to select their own tube lines etc...</p>
             <p>The London Underground data is provided by TfL and is updated every 30 seconds</p>
-            <p>The Home page will update itself every 31 seconds to display the most recent data. The way it updates means you won't notice unless a lines status changes</p>
+            <p>The Home page will update itself every 32 seconds to display the most recent data. The way it updates means you won't notice unless a lines status changes</p>
             <hr>
             <p>if you find an issue with the website, or would like to contact me for any other reason, please e-mail info@journeychecker.com</p>
             
--- index.php
+++ index.php
@@ -54,7 +54,28 @@
                 <button type="button" class="close" data-dismiss="alert">&times;</button>
                 <p><strong>Welcome to Journey Checker!</strong> Set your preferences to limit which lines you see below</p>
                 <h5>This website uses cookies to remember your preferences. By setting your preferences you are agreeing to allow this site to store cookies on your computer</h5>
-                <p><a class="btn btn-warning btn-raised" href="preferences">Set Preferences</a></p>
+                <p><a class="btn btn-warning btn-raised" href="preferences">Set Preferences</a>
+                    <button class="btn btn-primary btn-raised" data-toggle="modal" data-target="#info-dialog">More Information</button> </p>
+            </div>
+            <div id="info-dialog" class="modal fade" tabindex="-1">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                      <h2 class="modal-title">More Information</h2>
+                    </div>
+                    <div class="modal-body">
+                      <p>Journey Checker allows you to select what London Underground lines you use on your commute to and from work.</p>
+                       <p>Once this is set, only these lines will be displayed on the homepage, allowing you to quickly and easily view any delays on lines you use.</p>
+                       <p>So that you can see any delays as easily as possible, underground lines currently experiencing delays are shown in red. Any information provided by Transport for London (TfL) is included, and is updated automatically when TfL release new information about the delay. </p>
+                       <p>If you leave the page open it will automatically refresh the status of each line every 32 seconds.</p>
+                       <p>Journey Checker looks good on any screen size, making it ideal for viewing on your mobile as you travel. </p>
+                    </div>
+                    <div class="modal-footer">
+                      <button class="btn btn-primary" data-dismiss="modal">Dismiss</button>
+                    </div>
+                  </div>
+                </div>
             </div>
         </div>
         <?php } ?>
@@ -90,7 +111,7 @@
             }
         });
 
-        setInterval('refresh_lines()', 31000);
+        setInterval('refresh_lines()', 32000);
         function refresh_lines() {
             $( "#TfLlines" ).load( "/ajax/tfllines.php", function( response, status, xhr ) {
                 if ( status == "error" ) {
